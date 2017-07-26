f3d6bc737e3ea474374fb2ce9573e9e375553587
Huey Peard <hueypeard@gmail.com>
2017-07-26 19:21:31 +1000

    Changed template code

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div styleName="App">
+        <div styleName="App__header">
+          <Logo styleName="App__logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p styleName="App__intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
